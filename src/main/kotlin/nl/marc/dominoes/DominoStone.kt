a3cae62277a47d962b2526a712ac93b1f4bb8baf
Marc <16156117+Marc-JB@users.noreply.github.com>
2020-05-09 11:55:55 +0200

    Add DominoStone text parser

--- src/main/kotlin/nl/marc/dominoes/DominoStone.kt
+++ src/main/kotlin/nl/marc/dominoes/DominoStone.kt
@@ -1,5 +1,16 @@
 package nl.marc.dominoes
 
+import java.lang.IllegalArgumentException
+
 data class DominoStone(val left: Int, val right: Int) {
-    override fun toString() = "[ $left | $right ]"
+    override fun toString() = "[$left|$right]"
+
+    fun reversed() = DominoStone(right, left)
+
+    companion object {
+        fun fromText(text: String): DominoStone {
+            val (left, right) = """\[([0-6])\|([0-6])]""".toRegex().find(text)?.destructured ?: throw IllegalArgumentException()
+            return DominoStone(left.toInt(), right.toInt())
+        }
+    }
 }
---
+++ src/test/kotlin/nl/marc/dominoes/DominoStoneTests.kt
@@ -0,0 +1,32 @@
+package nl.marc.dominoes
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class DominoStoneTests {
+    @Test
+    fun testTextParser() {
+        // Arrange
+        val text = "[2|4]"
+        val expectedResult = DominoStone(2, 4)
+
+        // Act
+        val actualResult = DominoStone.fromText(text)
+
+        // Assert
+        assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun testTextParserReversed() {
+        // Arrange
+        val text = "[4|2]"
+        val expectedResult = DominoStone(2, 4).reversed()
+
+        // Act
+        val actualResult = DominoStone.fromText(text)
+
+        // Assert
+        assertEquals(expectedResult, actualResult)
+    }
+}
